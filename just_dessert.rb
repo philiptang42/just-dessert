c124dcec7552a10bd669e00d5e9ff1feb7e9e4b0
philiptang42 <philiptang42@gmail.com>
2016-01-08 23:42:12 -0500

    cook_batch! method progression

---
+++ just_dessert.rb
@@ -0,0 +1,13 @@
+class CookieInventory
+  attr_accessor :peanut_butter, :chocolate_chip, :sugar
+
+  def initialize(cookie)
+    @peanut_butter = peanut_butter
+    @chocolate_chip = chocolate_chip
+    @sugar = sugar
+  end
+
+  def cook_batch!
+  end
+  
+end
